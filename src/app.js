0480a1cdc0b428eb9446586629574c779e713dbc
Igor Napierala <igor.napierala@cognifide.com>
2021-01-06 15:22:59 +0100

    basic observables

--- src/app.js
+++ src/app.js
@@ -1,4 +1,20 @@
-import $ from "jquery";
-import Rx from "rxjs/Rx";
+import { Observable } from "rxjs/Rx";
 
-console.log("RxJS Boiler Running...");
+const btn = document.getElementById("btn");
+const input = document.getElementById("input");
+
+const btnStream$ = Observable.fromEvent(btn, "click");
+
+btnStream$.subscribe(
+  (e) => console.log(e.target.innerHTML),
+  (err) => console.log(err),
+  () => console.log("Completed")
+);
+
+const inputStream$ = Observable.fromEvent(input, "keyup");
+
+inputStream$.subscribe(
+  (e) => console.log(e),
+  (err) => console.log(err),
+  () => console.log("Completed")
+);
